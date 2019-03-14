6c17a13526a79f3376efd8b4d4894b71c28c809a
danielwoldanski <46328977+danielwoldanski@users.noreply.github.com>
2019-03-14 21:04:42 +0100

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,10 +1,11 @@
 import os
 
-file_list = os.listdir("D:\Python_lekcje")
-print(file_list)
+file_list = os.listdir("Ścieżka pliku / Path directory")
 save_path = os.getcwd()
-os.chdir("D:\Python_lekcje")
-print(os.getcwd())
-table = str.maketrans(dict.fromkeys('jpg'))
-for i in file_list:
-    os.rename(i, i.translate(table))
+os.chdir("Ścieżka pliku / Path directory")
+table = str.maketrans(dict.fromkeys('znaki jakie mają zostać usunięte / chars to remove'))
+
+for file_name in file_list:
+    os.rename(file_name, file_name.translate(table))
+ 
+os.chdir(save_path)
